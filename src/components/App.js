c4b7d5bf2b744099005c48a776ca4a5164c3127f
Rahil1910 <rahilrafique1234@gmail.com>
2025-02-23 13:05:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,41 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import { useState } from "react";
+import "./index.css";
+import { useEffect } from "react";
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+const App = ()=> {
+    const [started,setState] = useState(false);
+    const [pos,setPos] = useState(0);
+    const onStart=()=>{
+        setState(true);
+    }
+    const  onMove=()=>{
+        setPos((prevPos) => prevPos + 5);
+    }
+    const handleKeyPress= (event) => {
+        if(event.keyCode === 39){
+            onMove();
+        }
     };
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
-
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
+    useEffect(()=>{
+        window.addEventListener("keydown",handleKeyPress);
+        return ()=>{
+            window.removeEventListener("keydown",handleKeyPress);
+        };
+    },[]);
+    return(
+        <>
+            <div>
+                {!started &&(
+                    <button onClick={onStart} className="start">Start</button>
+                )}
+                {started && (
+                    <>
+                    <div style={{transform:`translate(${pos}px)`}} className="ball"></div>
+                    </>
+                )}
             </div>
-        )
-    }
+        </>
+    );
 }
-
-
 export default App;
